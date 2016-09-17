5dc214dbf078b26a5022f0ef42987d3d7a41e486
Ryan Haskell-Glatz <rhaskellglatz@onenorth.com>
2016-09-17 12:52:51 -0500

    remove favicon, upgrade to keystone 4

--- keystone.js
+++ keystone.js
@@ -1,23 +1,21 @@
 var keystone = require('keystone');
 
 keystone.init({
-	
+
 	'name': 'Lawyer & Sons',
-	
-	'favicon': 'public/favicon.ico',
-	
+		
 	'auto update': true,
 	'mongo': process.env.MONGO_URI || 'mongodb://mongo/lawyer-and-sons',
-	
+
 	'session': true,
 	'auth': true,
 	'user model': 'User',
 	'cookie secret': 'cookie-secret'
-	
+
 });
- 
+
 require('./models');
- 
+
 keystone.set('routes', require('./routes'));
- 
+
 keystone.start();
